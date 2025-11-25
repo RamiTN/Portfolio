0906a53886e5178549b0a375f9ec4c2e57d2ad29
RamiTN <ramiabbassi53@gmail.com>
2025-11-25 20:13:39 +0100

    fix sticky navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="navbar list-none flex gap-8 justify-center py-6 bg-gray-800 text-white">
+    <nav className="sticky top-0 navbar list-none flex gap-8 justify-center py-6 bg-gray-800 text-white absolute w-full z-10">
         <li><a href="#intro">Introduction</a></li>
         <li><a href="#stats">Statistics</a></li>
         <li><a href="#projects">Personel projects</a></li>
